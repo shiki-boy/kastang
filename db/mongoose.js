c8bd4b502a6149061d4a5bcd307f379c9264fa25
Raghav Kaushik <raghav.kaushik611@gmail.com>
2018-05-24 17:04:03 +0530

    mLab

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var userConn = mongoose.createConnection('mongodb://localhost:27017/WebDB');        // will itself create a database if is not there
-var productConn = mongoose.createConnection('mongodb://localhost:27017/productsDB');
+var userConn = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost:27017/WebDB');        // will itself create a database if is not there
+var productConn = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost:27017/productsDB');
 // var product_infoConn = mongoose.createConnection('mongodb://localhost:27017/ProductInfoDB');
 // var brandsConn = mongoose.createConnection('mongodb://localhost:27017/brandsDB');
 
